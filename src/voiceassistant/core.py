b304b583bb5c14a941ddff06d0f2efe1f8f90eb5
Vadim Titov <titov.hse@gmail.com>
2021-05-02 20:02:10 +0100

    Add error handling in the main loop

--- src/voiceassistant/core.py
+++ src/voiceassistant/core.py
@@ -24,8 +24,12 @@ def run_voice_assistant() -> None:
                 pass
             print("Hotword detected.")
             with NaturalLanguageProcessor() as nlp:
-                for transcript in speech.sst.recognize_from_stream(stream):
-                    print_and_flush(transcript)
-                    nlp.process_next_transcript(
-                        transcript=transcript, interface=speech
-                    )
+                try:
+                    for transcript in speech.sst.recognize_from_stream(stream):
+                        print_and_flush(transcript)
+                        nlp.process_next_transcript(
+                            transcript=transcript, interface=speech
+                        )
+                except Exception as e:
+                    print(e)
+                    speech.output("Error occured")
