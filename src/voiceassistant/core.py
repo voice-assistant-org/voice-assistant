cf110e64c692d7ece1568cda65fb2d4dfc1c77b0
Vadim Titov <titov.hse@gmail.com>
2021-09-13 22:35:39 +0100

    Structure re-design: core

--- src/voiceassistant/core.py
+++ src/voiceassistant/core.py
@@ -1,24 +1,39 @@
 """Voice Assistant core components."""
 
 import threading
+from typing import Callable, List
 
-import voiceassistant.skills  # NOQA
-from voiceassistant.interfaces import HttpInterface, SpeechInterface
+from voiceassistant.integrations import IntegrationsComponent
+from voiceassistant.interfaces import InterfacesComponent
+from voiceassistant.nlp import NaturalLanguageComponent
+from voiceassistant.skills import SkillsComponent
+
+VassJob = Callable[[], None]
 
 
 class VoiceAssistant:
     """Voice Assistant root class."""
 
+    _jobs: List[VassJob] = []
+
     def __init__(self) -> None:
-        """Initialize Voice Assistant components."""
-        self.speech = SpeechInterface()
-        self.http = HttpInterface(self)
+        """Initialize Voice Assistant components.
+
+        Order of initialization matters.
+        """
+        self.nlp = NaturalLanguageComponent(self)
+        self.interfaces = InterfacesComponent(self)
+        self.skills = SkillsComponent(self)
+        self.integrations = IntegrationsComponent(self)
+
+    def add_job(self, job: VassJob) -> None:
+        """Add job for Voice Assistant to run."""
+        self._jobs.append(job)
 
     def run(self) -> None:
         """Run Voice Assistant jobs in separate threads."""
-        jobs = (
-            self.speech.run,
-            self.http.run,
-        )
-        for job in jobs:
+        for job in self._jobs:
             threading.Thread(target=job).start()
+
+
+__all__ = ["VoiceAssistant"]
