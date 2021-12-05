05b23931584b1a7e118b733ee5f7d8498f775076
Vadim Titov <titov.hse@gmail.com>
2021-12-05 21:51:30 +0000

    Implement component reloading method for the core class

--- src/voiceassistant/core.py
+++ src/voiceassistant/core.py
@@ -5,12 +5,7 @@ from typing import Any, Callable, Dict, List
 
 import diskcache as dc
 
-from voiceassistant.addons import AddonsComponent
 from voiceassistant.const import CACHE_DIR
-from voiceassistant.integrations import IntegrationsComponent
-from voiceassistant.interfaces import InterfacesComponent
-from voiceassistant.nlp import NaturalLanguageComponent
-from voiceassistant.skills import SkillsComponent
 
 VassJob = Callable[[], None]
 
@@ -18,15 +13,30 @@ VassJob = Callable[[], None]
 class VoiceAssistant:
     """Voice Assistant root class."""
 
-    _jobs: List[VassJob] = []
-    data: Dict[str, Any] = {}  # data dict for components to share
-
     def __init__(self) -> None:
-        """Initialize Voice Assistant components.
+        """Initialize Voice Assistant."""
+        self._jobs: List[VassJob] = []
+
+        self.data: Dict[str, Any] = {}
+        self.cache: dc.Cache = dc.Cache(CACHE_DIR)
+        self.load_components()
+
+    def load_components(self) -> None:
+        """Import and initialize Voice Assistant components.
+
+        Importing is done here because we make sure that on every
+        call this function reloads components' modules too.
 
         Order of initialization matters.
         """
-        self.cache = dc.Cache(CACHE_DIR)
+        print("*** Loading Components ***")
+
+        from voiceassistant.addons import AddonsComponent
+        from voiceassistant.integrations import IntegrationsComponent
+        from voiceassistant.interfaces import InterfacesComponent
+        from voiceassistant.nlp import NaturalLanguageComponent
+        from voiceassistant.skills import SkillsComponent
+
         self.nlp = NaturalLanguageComponent(self)
         self.interfaces = InterfacesComponent(self)
         self.skills = SkillsComponent(self)
