a19c56c1dd96c0a51f4ef447c56dfee78cffd79c
Vadim Titov <titov.hse@gmail.com>
2022-02-06 13:58:05 +0000

    Update component reloading feature, add safety to add_job method

--- src/voiceassistant/core.py
+++ src/voiceassistant/core.py
@@ -6,6 +6,8 @@ from typing import Any, Callable, Dict, List
 import diskcache as dc
 
 from voiceassistant.const import CACHE_DIR
+from voiceassistant.exceptions import AssistantBaseException
+from voiceassistant.interfaces import InterfacesComponent
 
 VassJob = Callable[[], None]
 
@@ -16,9 +18,15 @@ class VoiceAssistant:
     def __init__(self) -> None:
         """Initialize Voice Assistant."""
         self._jobs: List[VassJob] = []
+        self._is_running = False
 
         self.data: Dict[str, Any] = {}
         self.cache: dc.Cache = dc.Cache(CACHE_DIR)
+
+        # interfaces must only be instantiated once, no reloading allowed
+        self.interfaces = InterfacesComponent(self)
+
+        # reloadable components
         self.load_components()
 
     def load_components(self) -> None:
@@ -33,22 +41,24 @@ class VoiceAssistant:
 
         from voiceassistant.addons import AddonsComponent
         from voiceassistant.integrations import IntegrationsComponent
-        from voiceassistant.interfaces import InterfacesComponent
         from voiceassistant.nlp import NaturalLanguageComponent
         from voiceassistant.skills import SkillsComponent
 
         self.nlp = NaturalLanguageComponent(self)
-        self.interfaces = InterfacesComponent(self)
         self.skills = SkillsComponent(self)
         self.addons = AddonsComponent(self)
         self.integrations = IntegrationsComponent(self)
 
     def add_job(self, job: VassJob) -> None:
         """Add job for Voice Assistant to run."""
+        if self._is_running:
+            raise AssistantBaseException("Cannot add job after Voice Assistant started running.")
+
         self._jobs.append(job)
 
     def run(self) -> None:
         """Run Voice Assistant jobs in separate threads."""
+        self._is_running = True
         for job in self._jobs:
             threading.Thread(target=job).start()
 
