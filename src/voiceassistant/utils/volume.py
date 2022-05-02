3e48f7fa5a29aa231913e995858d288ca9807bf9
Vadim Titov <titov.hse@gmail.com>
2022-05-02 14:08:07 +0100

    Add host device volume control utils

---
+++ src/voiceassistant/utils/volume.py
@@ -0,0 +1,46 @@
+from types import TracebackType
+from typing import Type
+
+import alsaaudio
+
+
+class _Mixer:
+    """Mixer with context handling."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Init."""
+        self._alsamixer = alsaaudio.Mixer(*args, **kwargs)
+
+    def __enter__(self) -> alsaaudio.Mixer:
+        """Enter."""
+        return self._alsamixer
+
+    def __exit__(
+        self, type: Type[BaseException], value: BaseException, traceback: TracebackType
+    ) -> None:
+        """Close mixer."""
+        self._alsamixer.close()
+
+
+def set_volume(level: int) -> None:
+    """Set master volume."""
+    with _Mixer() as mixer:
+        mixer.setvolume(level)
+
+
+def get_volume() -> int:
+    """Get master volume."""
+    with _Mixer() as mixer:
+        return mixer.getvolume()[0]
+
+
+def set_mute(mute: bool) -> None:
+    """Set mute, 1 for mute and 0 for unmute."""
+    with _Mixer() as mixer:
+        mixer.setmute(int(mute))
+
+
+def is_muted() -> bool:
+    """Check if volume is muted."""
+    with _Mixer() as mixer:
+        return 0 not in mixer.getmute()
